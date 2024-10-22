951a6b5b566fa97cb9407f2451e5d2a65bd07364
tshun <s.t0811@icloud.com>
2024-10-22 18:22:08 +0900

    Initial Commit

---
+++ CustomVideoPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomVideoPlayer
+//
+//  Created by 田島隼也 on 2024/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
